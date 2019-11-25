55447213e7286dd7d659291aee52ba4e6a6710bb
bveknatr <bvenkatr2@gmail.com>
2019-11-25 10:25:06 +0530

    replace video player with dash player

--- videojs/videojs.py
+++ videojs/videojs.py
@@ -7,6 +7,7 @@ from xblock.core import XBlock
 from xblock.fields import Scope, Integer, String, Boolean
 from xblock.fragment import Fragment
 
+
 class videojsXBlock(XBlock):
 
     '''
@@ -18,49 +19,51 @@ class videojsXBlock(XBlock):
     Fields
     '''
     display_name = String(display_name="Display Name",
-        default="Video JS",
-        scope=Scope.settings,
-        help="This name appears in the horizontal navigation at the top of the page.")
+                          default="Video JS",
+                          scope=Scope.settings,
+                          help="This name appears in the horizontal navigation at the top of the page.")
 
     url = String(display_name="Video URL",
-        default="http://vjs.zencdn.net/v/oceans.mp4",
-        scope=Scope.content,
-        help="The URL for your video.")
-    
+                 default="https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd",
+                 scope=Scope.content,
+                 help="The URL for your video.")
+
     allow_download = Boolean(display_name="Video Download Allowed",
-        default=True,
-        scope=Scope.content,
-        help="Allow students to download this video.")
-    
+                             default=True,
+                             scope=Scope.content,
+                             help="Allow students to download this video.")
+
     source_text = String(display_name="Source document button text",
-        default="",
-        scope=Scope.content,
-        help="Add a download link for the source file of your video. Use it for example to provide the PowerPoint or PDF file used for this video.")
-    
+                         default="",
+                         scope=Scope.content,
+                         help="Add a download link for the source file of your video. Use it for example to provide the PowerPoint or PDF file used for this video.")
+
     source_url = String(display_name="Source document URL",
-        default="",
-        scope=Scope.content,
-        help="Add a download link for the source file of your video. Use it for example to provide the PowerPoint or PDF file used for this video.")
-    
+                        default="",
+                        scope=Scope.content,
+                        help="Add a download link for the source file of your video. Use it for example to provide the PowerPoint or PDF file used for this video.")
+
     start_time = String(display_name="Start time",
-        default="",
-        scope=Scope.content,
-        help="The start and end time of your video. Equivalent to 'video.mp4#t=startTime,endTime' in the url.")
-    
+                        default="",
+                        scope=Scope.content,
+                        help="The start and end time of your video. Equivalent to 'video.mp4#t=startTime,endTime' in the url.")
+
     end_time = String(display_name="End time",
-        default="",
-        scope=Scope.content,
-        help="The start and end time of your video. Equivalent to 'video.mp4#t=startTime,endTime' in the url.")
+                      default="",
+                      scope=Scope.content,
+                      help="The start and end time of your video. Equivalent to 'video.mp4#t=startTime,endTime' in the url.")
 
     '''
     Util functions
     '''
+
     def load_resource(self, resource_path):
         """
         Gets the content of a resource
         """
-        resource_content = pkg_resources.resource_string(__name__, resource_path)
-        return unicode(resource_content)
+        resource_content = pkg_resources.resource_string(
+            __name__, resource_path)
+        return unicode(resource_content, 'utf-8')
 
     def render_template(self, template_path, context={}):
         """
@@ -72,6 +75,7 @@ class videojsXBlock(XBlock):
     '''
     Main functions
     '''
+
     def student_view(self, context=None):
         """
         The primary view of the XBlock, shown to students
@@ -84,7 +88,7 @@ class videojsXBlock(XBlock):
             fullUrl += "#t=" + self.start_time
         elif self.end_time != "":
             fullUrl += "#t=0," + self.end_time
-        
+
         context = {
             'display_name': self.display_name,
             'url': fullUrl,
@@ -92,12 +96,17 @@ class videojsXBlock(XBlock):
             'source_text': self.source_text,
             'source_url': self.source_url
         }
+
         html = self.render_template('static/html/videojs_view.html', context)
-        
+
         frag = Fragment(html)
-        frag.add_css(self.load_resource("static/css/video-js.min.css"))
         frag.add_css(self.load_resource("static/css/videojs.css"))
-        frag.add_javascript(self.load_resource("static/js/video-js.js"))
+
+        '''
+        No need to load dash.all.min.js as I have already added it from cdn in videojs_view.html
+        frag.add_javascript(self.load_resource("static/js/dash.all.debug.js"))
+        <script src="https://cdn.dashjs.org/latest/dash.all.min.js"></script>
+        '''
         frag.add_javascript(self.load_resource("static/js/videojs_view.js"))
         frag.initialize_js('videojsXBlockInitView')
         return frag
@@ -117,7 +126,7 @@ class videojsXBlock(XBlock):
             'end_time': self.end_time
         }
         html = self.render_template('static/html/videojs_edit.html', context)
-        
+
         frag = Fragment(html)
         frag.add_javascript(self.load_resource("static/js/videojs_edit.js"))
         frag.initialize_js('videojsXBlockInitStudio')
@@ -130,12 +139,14 @@ class videojsXBlock(XBlock):
         """
         self.display_name = data['display_name']
         self.url = data['url']
-        self.allow_download = True if data['allow_download'] == "True" else False # Str to Bool translation
+        # Str to Bool translation
+        self.allow_download = True if data['allow_download'] == "True" else False
         self.source_text = data['source_text']
         self.source_url = data['source_url']
-        self.start_time = ''.join(data['start_time'].split()) # Remove whitespace
-        self.end_time = ''.join(data['end_time'].split()) # Remove whitespace
-        
+        self.start_time = ''.join(
+            data['start_time'].split())  # Remove whitespace
+        self.end_time = ''.join(data['end_time'].split())  # Remove whitespace
+
         return {
             'result': 'success',
         }
